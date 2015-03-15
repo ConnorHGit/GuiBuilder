78907d391992a19c1a01767adcc191e2db55697c
Isaiah Ballah <isaiahballah@gmail.com>
2015-03-14 20:12:07 -0700

    DockableWindow[WIP]

---
+++ GuiBuilder/Editor Framework/Windows/DockableWindow.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GuiBuilder.Editor_Framework.Windows
+{
+	public class DockableWindow
+	{
+		private Window window;
+		private Panel panel;
+		private Boolean docked;
+		private List<DockableWindow> right, left, up, down;
+
+		
+	}
+
+	public enum DockPosition
+	{
+		up, down, left, right
+	}
+}
--- GuiBuilder/Form1.Designer.cs
+++ GuiBuilder/Form1.Designer.cs
@@ -29,6 +29,7 @@
 		 private void InitializeComponent()
 		 {
 			this.label1 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
 			this.SuspendLayout();
 			// 
 			// label1
@@ -42,12 +43,24 @@
 			this.label1.TabIndex = 0;
 			this.label1.Text = "Awesome Editor";
 			// 
+			// textBox1
+			// 
+			this.textBox1.AcceptsReturn = true;
+			this.textBox1.AcceptsTab = true;
+			this.textBox1.Location = new System.Drawing.Point(12, 108);
+			this.textBox1.Multiline = true;
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(240, 141);
+			this.textBox1.TabIndex = 1;
+			this.textBox1.WordWrap = false;
+			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.BackColor = System.Drawing.SystemColors.Control;
 			this.ClientSize = new System.Drawing.Size(284, 261);
+			this.Controls.Add(this.textBox1);
 			this.Controls.Add(this.label1);
 			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
 			this.Name = "Form1";
@@ -61,6 +74,7 @@
 		 #endregion
 
 		 private System.Windows.Forms.Label label1;
+		 private System.Windows.Forms.TextBox textBox1;
 
 	 }
  }
